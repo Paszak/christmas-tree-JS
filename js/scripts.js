1637073e951e4b39f85384911ae388e57162bb28
Paweł Koleśnik <pawelkolesnik@wp.pl>
2017-11-29 18:23:49 +0100

    do task

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
-var text = ('Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.');
-var dinosaur = ('triceratops');
-dinosaur = dinosaur.toUpperCase();
-text = text.replace('Velociraptor', dinosaur);
-console.log(text.substr(0, text.length/2));
+function drawTree (heightTree) {
+	var star;
+	for (var i = 0 ; i < heightTree ; i++) {
+		star = "";
+		
+		for (var j = 0 ; j < i ; j++) {
+			star += "* ";
+		}
+
+		console.log(star);
+	}
+}
