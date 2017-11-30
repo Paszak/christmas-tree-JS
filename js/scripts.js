1ae5ed279dfae8f85aefbfdb3b584b73e1d5b5de
Paweł Koleśnik <pawelkolesnik@wp.pl>
2017-11-30 18:09:53 +0100

    center chrismas tree

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 function drawTree (heightTree) {
 	var star;
-	for (var i = 0 ; i < heightTree ; i++) {
+	for (var i = 1 ; i <= heightTree ; i++) {
 		star = "";
 		
-		for (var j = 0 ; j < i ; j++) {
-			star += "* ";
+		for (var j = i ; j < heightTree ; j++) {
+			star += " ";
 		}
 
+		for(k=0;k<(i*2)-1;k++) {
+   			 star += "*";
+  		}
 		console.log(star);
 	}
 }
